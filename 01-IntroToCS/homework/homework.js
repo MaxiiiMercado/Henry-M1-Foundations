b16c67b2fea903f77b73abd770b2728ed8b69470
MakroMercado <makrodevjs@gmail.com>
2022-08-12 00:52:52 -0300

    homework - IntroToCS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,19 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  return num.split('').reverse().reduce( (decimal, binario, index) => parseInt(decimal) + binario * (2 ** index));
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binario = '';
 
+  while (num > 0){
+    binario = (num % 2) + binario;
+    num = parseInt(num / 2); 
+  }
+
+  return binario;
 }
 
 

44a0f095a76cd7559688587587bfbb3e06a9d142
MaxiiiMercado95 <MaxiiiMercado95@gmail.com>
2022-09-02 23:37:47 -0300

    homework - Algoritmos-II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,27 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length < 2)
+    return array;
+  else{
+    let pivote = parseInt(Math.random() * array.length);
+    let arrayL = [];
+    let arrayR = [];
+    for (let i = 0; i < array.length; i++) {
+      if (i != pivote)
+        array[i] <= array[pivote] ? arrayL.push(array[i]) : arrayR.push(array[i])
+    }
+    return quickSort(arrayL).concat(array[pivote],quickSort(arrayR))
+  }
+}
 
+
+function merge(arrayL, arrayR){
+  let arrayN = [];
+  while (arrayL.length && arrayR.length){
+    arrayL[0] < arrayR[0] ? arrayN.push(arrayL.shift()) : arrayN.push(arrayR.shift())
+  }
+  return arrayN.concat(arrayL, arrayR);
 }
 
 function mergeSort(array) {
@@ -14,7 +34,13 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length < 2)
+    return array;
+  else{
+    let mitadL = array.slice(0, parseInt(array.length / 2));
+    let mitadR = array.slice(parseInt(array.length / 2));
+    return merge(mergeSort(mitadL), mergeSort(mitadR));
+  }
 }
 
 // No modificar nada debajo de esta línea
